6a799f65fc5a3853010071b4d15f7c9e73c42aaf
Linus Kåge <linka360@student.liu.segit config --global --listgit config --global user.email linka360@student.liu.se>
2021-10-09 14:04:39 +0200

    Fixat kommentarer till greedy

--- R/greedy_knapsack.R
+++ R/greedy_knapsack.R
@@ -1,3 +1,22 @@
+#' Greedy knapsack solution of the knapsack problem
+#'
+#' @param x x is a data.frame containing two columns, w and v
+#' @param W W is the capacity of the knapsack and is an integer greater than 0.
+#'
+#' @return Returns a list containing the maximum value of combination of the objects and the objects in the combination.
+#' @export
+#'
+#' @examples
+#' #' #'suppressWarnings(RNGversion(min(as.character(getRversion()),"3.5.3")))
+#'set.seed(42, kind = "Mersenne-Twister", normal.kind = "Inversion")
+#'n <- 2000
+#'knapsack_objects <-
+#'  data.frame(
+#'    w=sample(1:4000, size = n, replace = TRUE),
+#'    v=runif(n = n, 0, 10000)
+#'  )
+#'
+#' greedy_knapsack(knapsack_objects[1:800,], W = 3500)
 greedy_knapsack <- function(x, W){
 
 
